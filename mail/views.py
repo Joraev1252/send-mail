3d2c9b690c19001b50bdfdbf634dc02a18c4d1a8
Joraev Azam <joraev.azam1252@gmail.com>
2022-09-16 17:49:49 +0500

    done

--- mail/views.py
+++ mail/views.py
@@ -59,7 +59,7 @@ print(datetime.now().strftime("%H:%M:%S"))
 
 while True:
     now = datetime.now().strftime("%H:%M:%S")
-    given_time = "17:49:00"
+    given_time = "17:51:00"
 
     if given_time == now:
         print("******************")
