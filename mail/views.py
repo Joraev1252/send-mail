ddb4c5513afa5b01598738410c7681c59eb3d069
Joraev Azam <joraev.azam1252@gmail.com>
2022-09-18 16:52:38 +0500

    well done

--- mail/views.py
+++ mail/views.py
@@ -1,5 +1,7 @@
 import smtplib
 import math
+import threading
+
 from mail.tests import password
 from datetime import datetime, time
 from threading import Timer
@@ -17,24 +19,9 @@ current_period = now.strftime("%p")
 
 
 def send_mail():
-    if datetime.now().strftime("%H:%M") == "09:00":
-        body = f"Dhuhur Prayer Time 12:16 PM"
-        message = f'Subject: {theme}\n\n{body}\n\n{sender}'
-        server = smtplib.SMTP("smtp.mail.ru", 587)
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
-        server.login(sender, password)
-        server.sendmail(sender, reciever, message)
-        server.quit()
-        print("Succesfully! 1")
-    else:
-        hour_delta = int(current_hour) - 9
-        min_delta = int(current_min) - 0
-        sec_delta = int(current_sec) - 5
-
-        if hour_delta != 0:
-            body = f"Dhuhur Prayer Time 12:16 PM, Sorry we are {round(math.fabs(hour_delta))} hour {round(math.fabs(min_delta))} minutes late due to technical issues."
+    try:
+        if datetime.now().strftime("%H:%M") == "17:00":
+            body = f"Dhuhur Prayer Time 12:16 PM"
             message = f'Subject: {theme}\n\n{body}\n\n{sender}'
             server = smtplib.SMTP("smtp.mail.ru", 587)
             server.ehlo()
@@ -43,36 +30,60 @@ def send_mail():
             server.login(sender, password)
             server.sendmail(sender, reciever, message)
             server.quit()
-            print("Succesfully! 2")
+            print("Succesfully! 1")
+        else:
+            hour_delta = int(current_hour) + 12 - 17
+            min_delta = int(current_min) - 0
+            sec_delta = int(current_sec) - 5
 
-        elif hour_delta == 0 and min_delta != 0:
-            body = f"Dhuhur Prayer Time 12:16 PM, Sorry we are {round(math.fabs(min_delta))} minutes late due to technical issues."
-            message = f'Subject: {theme}\n\n{body}\n\n{sender}'
-            server = smtplib.SMTP("smtp.mail.ru", 587)
-            server.ehlo()
-            server.starttls()
-            server.ehlo()
-            server.login(sender, password)
-            server.sendmail(sender, reciever, message)
-            server.quit()
-            print("Succesfully! 3")
+            if hour_delta != 0:
+                body = f"Dhuhur Prayer Time 12:16 PM, Sorry we are {round(math.fabs(hour_delta))} hour {round(math.fabs(min_delta))} minutes late due to technical issues."
+                message = f'Subject: {theme}\n\n{body}\n\n{sender}'
+                server = smtplib.SMTP("smtp.mail.ru", 587)
+                server.ehlo()
+                server.starttls()
+                server.ehlo()
+                server.login(sender, password)
+                server.sendmail(sender, reciever, message)
+                server.quit()
+                print("Succesfully! 2")
 
-        elif hour_delta == 0 and min_delta == 0 and sec_delta != 0:
-            body = f"Dhuhur Prayer Time 12:16 PM, Sorry we are {round(math.fabs(sec_delta))} seconds late due to technical issues."
-            message = f'Subject: {theme}\n\n{body}\n\n{sender}'
-            server = smtplib.SMTP("smtp.mail.ru", 587)
-            server.ehlo()
-            server.starttls()
-            server.ehlo()
-            server.login(sender, password)
-            server.sendmail(sender, reciever, message)
-            server.quit()
-            print("Succesfully! 4")
+            elif hour_delta == 0 and min_delta != 0:
+                body = f"Dhuhur Prayer Time 12:16 PM, Sorry we are {round(math.fabs(min_delta))} minutes late due to technical issues."
+                message = f'Subject: {theme}\n\n{body}\n\n{sender}'
+                server = smtplib.SMTP("smtp.mail.ru", 587)
+                server.ehlo()
+                server.starttls()
+                server.ehlo()
+                server.login(sender, password)
+                server.sendmail(sender, reciever, message)
+                server.quit()
+                print("Succesfully! 3")
+
+            elif hour_delta == 0 and min_delta == 0 and sec_delta != 0:
+                body = f"Dhuhur Prayer Time 12:16 PM, Sorry we are {round(math.fabs(sec_delta))} seconds late due to technical issues."
+                message = f'Subject: {theme}\n\n{body}\n\n{sender}'
+                server = smtplib.SMTP("smtp.mail.ru", 587)
+                server.ehlo()
+                server.starttls()
+                server.ehlo()
+                server.login(sender, password)
+                server.sendmail(sender, reciever, message)
+                server.quit()
+                print("Succesfully! 4")
+    except:
+        print("Some thing wrong!")
 
 
-Timer(
-    (datetime.combine(
-        datetime.today(), time(9, 0, 5)
-    ) - datetime.now()).total_seconds(),
-    send_mail
-).start()
+threading.Timer((datetime.combine(datetime.today(), time(17, 0, 5)) - datetime.now()).total_seconds(), send_mail).start()
+
+#
+# def set_interval(func):
+#     def func_wrapper():
+#         set_interval(func)
+#         func()
+#     t = threading.Timer((datetime.combine(datetime.today(), time(9, 55, 5)) - datetime.now()).total_seconds(), func).start()
+#     return t
+#
+#
+# set_interval(send_mail)
